27e81a9ee9456dadf9b6fb805e7f39abd0eb2b11
SergeyR <sergey17232@mail.ru>
2026-03-17 22:21:08 +0300

    backend/fix: Добавлен импорт роутера расписания в главный файл

--- Backend/main.py
+++ Backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.jwt_auth.router import router as jwt_router
 from api.admin.router import router as admin_router
+from api.schedule.router import router as schedule_router
 import uvicorn
 
 app = FastAPI(
@@ -20,6 +21,7 @@ app.add_middleware(
 
 app.include_router(jwt_router)
 app.include_router(admin_router)
+app.include_router(schedule_router)
 
 @app.get(
     "/healthcheck",
