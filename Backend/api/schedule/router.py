367661fa03bf411c5375bedc85a9392df80b5359
SergeyR <sergey17232@mail.ru>
2026-03-17 21:31:53 +0300

    backend/feat(schedule): Задана изначальная логика получения расписания
    - создан endpoint /api/schedule
    - добавлена поддержка offset для получения расписания на другой день

---
+++ Backend/api/schedule/router.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from datetime import datetime, timezone, timedelta
+
+from core.database import database
+from core.models import User
+from .schemas import ScheduleItemOut
+from .crud import get_schedule_items_for_day
+from api.jwt_auth.dependencies import get_current_user
+
+router = APIRouter(prefix="/api", tags=["Schedule"])
+
+
+@router.get("/schedule", response_model=list[ScheduleItemOut])
+async def get_schedule(
+    offset: int = 0,
+    user: User = Depends(get_current_user),
+    session: AsyncSession = Depends(database.get_session),
+):
+    target_day = datetime.now(timezone.utc) + timedelta(days=offset)
+
+    if user.group_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User does not consist in a group",
+        )
+
+    items = await get_schedule_items_for_day(session, user.group_id, target_day)
+    return items
